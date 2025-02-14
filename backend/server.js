cb7f6550905fa956eaf78c8c9e95ce9cfb401e4a
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2025-02-14 19:58:11 +0530

    setup authRoutes

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,19 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./db/connectDB");
+const authRoutes = require("./routes/auth.routes.js");
 
 const app = express();
 
 dotenv.config();
 app.use(cookieParser());
+app.use(express.json());
 
 connectDB();
 
 const PORT = process.env.PORT || 8000;
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
