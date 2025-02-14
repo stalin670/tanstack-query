a262a34016bdd2d12dee630a3252920720b23eaf
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2025-02-14 22:32:02 +0530

    implemented logout-logic

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -83,6 +83,8 @@ const login = async (req, res) => {
 
 const logout = async (req, res) => {
   try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    return res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     return res.status(500).json({ error: "Internal server error" });
   }
