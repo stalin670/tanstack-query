ac149b27cd82c098453c494264c91d7b51739e2c
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2025-02-15 01:35:56 +0530

    handle cors-policy

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,19 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./db/connectDB");
 const authRoutes = require("./routes/auth.routes.js");
+const cors = require("cors");
 
 const app = express();
 
 dotenv.config();
 app.use(cookieParser());
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 connectDB();
 
