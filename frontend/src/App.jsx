36db0a2fd4b3c548c677af05c074c231baa6b3db
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2025-02-15 00:23:42 +0530

    created logout-ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
+      <Home />
       {/* <Signup /> */}
-      <Login />
+      {/* <Login /> */}
     </div>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import "../style/Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div className="home-container">
+    <button className="logout-btn">Logout</button>
+  </div>;
 };
 
 export default Home;
